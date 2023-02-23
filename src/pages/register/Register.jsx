51bc055a72dea7b45b0a3bc988e4520c5844abbd
ktaeeun <ktaeeun43@gmail.com>
2023-02-23 09:51:18 +0900

    0223 수정

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import "./register.css";
+
+export default function Register() {
+  return (
+    <>
+      <div className="register">
+        <span className="registerTitle">Register</span>
+        <form className="registerForm">
+          <label>Username</label>
+          <input type="text" placeholder="Enter your name" />
+          <label>Email</label>
+          <input type="text" placeholder="Enter your email" />
+          <label>Password</label>
+          <input type="password" placeholder="Enter your password" />
+          <button className="registerButton">Register</button>
+        </form>
+        <button className="registerLoginButton">
+          <Link className="link" to="/login">
+            Login
+          </Link>
+        </button>
+      </div>
+    </>
+  );
+}
